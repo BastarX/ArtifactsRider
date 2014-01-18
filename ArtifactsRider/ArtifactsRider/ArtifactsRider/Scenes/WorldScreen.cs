e290abaabab550c996e88eee2b499ee61ea83cd2
Piotr B <pio4426@gmail.com>
2014-01-18 15:03:48 +0100

    World screen

---
+++ ArtifactsRider/ArtifactsRider/ArtifactsRider/Scenes/WorldScreen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using VAPI;
+
+
+namespace ArtifactsRider.Scenes
+{
+    class WorldScreen : GameScreen
+    {
+
+        public WorldScreen(Game Game)
+            : base(Game, GeneralManager.ScreenX, GeneralManager.ScreenY)
+        {
+            this.AddGUI(new Button(new Rectangle(100, 100, 100, 100), "", GeneralManager.Textures["Textures/TestTex"], Color.Gray, Color.White, "Fonts/SteamWreck"));
+        }
+   
+    }
+}
